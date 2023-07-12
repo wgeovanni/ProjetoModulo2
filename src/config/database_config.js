9dfdb86a02a885abe11105bb96d6ad829e98de0f
wgeovanni <willyangeo@gmail.com>
2023-07-11 22:44:10 -0300

    feat: Criação dos arquivos database_config, .sequelizerc, .env_example e connection.js, definição das variáveis de ambiente, criação da classe server no arquivo server.js e instanciação no arquivo index.js

---
+++ src/config/database_config.js
@@ -0,0 +1,24 @@
+// Importação de módulo para uso de arquivos .env
+const { config } = require('dotenv');
+config();
+
+const { DIALECT, HOST, USERNAMEDB, PASSWORDDB, DATABASE, PORT, SECRET_KEY } = process.env;
+
+module.exports = {
+
+    dialect: DIALECT,       // Recebe o tipo de banco de dados usado
+    host: HOST,             // Recebe qual servidor está sendo usado
+    username: USERNAMEDB,   // Nome do usuário no Postgres
+    password: PASSWORDDB,   // Senha do usuário no Postgres
+    database: DATABASE,     // Nome do banco de dados usado
+    port: PORT,             // Porta para conexão com banco de dados
+    secret: SECRET_KEY,     // Palavra-chave do token
+
+    //Configurações opcionais da aplicação
+    define: {
+
+        underscored: true,              // Traduz os campos para snake_case
+        underscoredAll: true,           // Traduz todos os campos para snake_case
+        paranoid: true                  // Permite o uso da exclusão lógica
+    }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+
+const routes = new Router();
+
+// Criação de rotas
+routes.use('/api');
+
+module.exports = routes;
