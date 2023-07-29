68ff1671a5fed0bdccbe2f24d8b184365b09a380
wgeovanni <willyangeo@gmail.com>
2023-07-29 15:52:35 -0300

    feat: S13 - Cadastro de medicamentos
    Criação da model da tabela satélite entre usuários, depósitos e
    medicamentos.

---
+++ src/models/usuario_deposito_medicamento.js
@@ -0,0 +1,48 @@
+const { STRING, INTEGER } = require('sequelize');
+const { connection } = require('../database/connection');
+
+const Usuarios_Depositos_Medicamentos = connection.define('usuarios_depositos_medicamentos', {
+
+    userId: {
+        type: INTEGER,
+        references: {
+            model: 'usuarios',
+            key: 'id'
+        },
+        allowNull: false
+    },
+    depositoId: {
+        type: INTEGER,
+        references: {
+            model: 'depositos',
+            key: 'key'
+        },
+        allowNull: false
+    },
+    medicamentoID: {
+        type: INTEGER,
+        references: {
+            model: 'medicamentos',
+            key: 'id'
+        },
+        allowNull: false
+    },
+    quantidade: {
+        type: INTEGER,
+        allowNull: false
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+    }
+}, { underscored: true, paranoid: true });
+
+module.exports = { Usuarios_Depositos_Medicamentos };
