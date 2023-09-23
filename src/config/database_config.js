209555d130b8cf151d007e45858727190deedc3c
wgeovanni <willyangeo@gmail.com>
2023-09-22 21:42:46 -0300

    fix: Mudança de enviroment keys

--- src/config/database_config.js
+++ src/config/database_config.js
@@ -2,22 +2,17 @@
 const { config } = require('dotenv');
 config();
 
-const { DIALECT, HOSTDB, USERNAMEDB, PASSWORDDB, DATABASE, PORTDB, SECRET_KEY } = process.env;
+const { DIALECT, PGHOST, PGUSER, PGPASSWORD, PGDATABASE, PGPORT, SECRET_KEY } = process.env;
 
 module.exports = {
 
     dialect: DIALECT,       // Recebe o tipo de banco de dados usado
-    host: HOSTDB,             // Recebe qual servidor está sendo usado
-    username: USERNAMEDB,   // Nome do usuário no Postgres
-    password: PASSWORDDB,   // Senha do usuário no Postgres
-    database: DATABASE,     // Nome do banco de dados usado
-    port: PORTDB,             // Porta para conexão com banco de dados
+    host: PGHOST,             // Recebe qual servidor está sendo usado
+    username: PGUSER,   // Nome do usuário no Postgres
+    password: PGPASSWORD,   // Senha do usuário no Postgres
+    database: PGDATABASE,     // Nome do banco de dados usado
+    port: PGPORT,             // Porta para conexão com banco de dados
     secret: SECRET_KEY,     // Palavra-chave do token
-    dialectOptions: {
-        ssl: {
-            require: false
-        }
-    },     // Palavra-chave do token
 
     //Configurações opcionais da aplicação
     define: {
